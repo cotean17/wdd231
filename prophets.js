5b5ff71e41ef96d3855132bf9a9fd1438730c1bc
mariajosemackay <mariajoarevalon@gmail.com>
2025-03-18 23:16:09 -0600

    w2

---
+++ prophets.js
@@ -0,0 +1,38 @@
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+const cards = document.querySelector('#cards');
+
+const getProphetData = async () => {
+  const response = await fetch(url);
+  const data = await response.json();
+  // console.table(data.prophets);
+  displayProphets(data.prophets);
+};
+
+const displayProphets = (prophets) => {
+  prophets.forEach((prophet, index) => {
+    const card = document.createElement('section');
+    const fullName = document.createElement('h2');
+    const birthDate = document.createElement('p');
+    const birthPlace = document.createElement('p');
+    const portrait = document.createElement('img');
+
+    fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+    birthDate.textContent = `Date of Birth: ${prophet.birthdate}`;
+    birthPlace.textContent = `Place of Birth: ${prophet.birthplace}`;
+
+    portrait.setAttribute('src', prophet.imageurl);
+    portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname} – ${index + 1}th Latter-day President`);
+    portrait.setAttribute('loading', 'lazy');
+    portrait.setAttribute('width', '340');
+    portrait.setAttribute('height', '440');
+
+    card.appendChild(fullName);
+    card.appendChild(birthDate);
+    card.appendChild(birthPlace);
+    card.appendChild(portrait);
+
+    cards.appendChild(card);
+  });
+};
+
+getProphetData();
